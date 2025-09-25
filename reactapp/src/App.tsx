d1b05fe6cc1e1dee114e5a23002eb8b6fc41eaa4
Shouwang Huang <shouwang@Shouwangs-MacBook-Air.local>
2025-09-25 09:39:42 -0400

    basic game complete

--- reactapp/src/App.tsx
+++ reactapp/src/App.tsx
@@ -1,21 +1,36 @@
 import { useState } from 'react'
 import './App.css'
+import { initialGameState,makeMove } from './connectfour'
 
 function App() {
+  const [gameState, setGameState] = useState(initialGameState)
 
   var space:number[] = []
   
-  for (var i = 41; i>=0; i--){
+  for (var i = 0; i<=41; i++){
     space.push(i)
   }
 
+  function playMove (position:number) {
+    setGameState(makeMove(position, gameState))
+  }
+
+  const statusClass = new Map<string, string>()
+    statusClass.set('red', "aspect-square border-2 rounded-4xl border-blue-700 bg-yellow-200")
+    statusClass.set('yellow',  "aspect-square border-2 rounded-4xl border-blue-700 bg-red-500")
+    statusClass.set('none', "aspect-square border-2 rounded-4xl border-blue-700 bg-amber-50")
+
   return (
-    <>
-      <div className="w-[400px] h-[400px] grid grid-cols-7 grid-rows-6">
-        {space.map(e=><div key={e}>{e}</div>)}
+    <div className="bg-black flex items-center justify-center w-screen h-screen">
+      <div className="w-200 h-200 grid grid-flow-col grid-rows-6">
+        {space.map(e=><div className={statusClass.get(gameState.board[e])}
+       
+        key={e} onClick={()=>{playMove(e)}}></div>)}
+      <button className="border rounded-2xl p-2 m-4 bg-blue-900 text-blue-300" onClick={()=>setGameState(initialGameState)}>Reset</button>
       </div>
 
-    </>
+    
+    </div>
   )
 }
 
---
+++ reactapp/src/connectfour.ts
@@ -0,0 +1,133 @@
+type GameState = {
+    currentPlayer: Player
+    board: string[]
+    winner: Player | undefined
+}
+
+type Player = 'red' | 'yellow'
+
+
+
+export const initialGameState: GameState = {
+    currentPlayer: 'red',
+    winner: undefined,
+    board: Array(42).fill('none')
+}
+
+export function makeMove(position: number, gameState: GameState): GameState {
+    if (gameState.winner) {
+        return gameState
+    }
+    let currentGS = structuredClone(gameState)
+    let num = position
+    let drop = Math.floor(num / 6)
+    for (let i = 5 + drop * 6; i >= drop * 6; i--) {
+        if (currentGS.board[i] === 'none') {
+            currentGS.board[i] = currentGS.currentPlayer
+            console.log(i)
+            currentGS = checkWin(currentGS)
+            currentGS.currentPlayer = (gameState.currentPlayer == 'red') ? 'yellow' : 'red'
+            return currentGS
+        }
+    }
+    return currentGS
+}
+
+
+function checkWin(gameState: GameState): GameState {
+    let currentGS = structuredClone(gameState)
+    if (checkDiagonalLeft(gameState) || checkDiagonalRight(gameState) ||
+        checkHorizontal(gameState) || checkVertical(gameState)) {
+        currentGS.winner = gameState.currentPlayer
+    }
+    return currentGS
+
+}
+
+function checkHorizontal(gameState: GameState): boolean {
+    for (var increment = 0; increment <= 6; increment++) {
+        var rowCount = 0
+        for (var i = 0 + increment; i <= 36 + increment; i += 6)
+            if (gameState.board[i] == gameState.currentPlayer) {
+                rowCount++
+                if (rowCount === 4) {
+                    return true
+                }
+            } else {
+                rowCount = 0
+            }
+    }
+    return false
+}
+
+function checkVertical(gameState: GameState): boolean {
+    for (var increment = 0; increment <= 36; increment += 6) {
+        var rowCount = 0
+        for (var i = 0 + increment; i <= 5 + increment; i++)
+            if (gameState.board[i] == gameState.currentPlayer) {
+                rowCount++
+                if (rowCount === 4) {
+                    return true
+                }
+            } else {
+                rowCount = 0
+            }
+    }
+    return false
+}
+
+function checkDiagonalRight(gameState: GameState): boolean {
+    for (var increment = 0; increment <= 2; increment++) {
+        var rowCount = 0
+        for (var i = 3 + increment; i <= 18 + increment * 6; i += 5)
+            if (gameState.board[i] == gameState.currentPlayer) {
+                rowCount++
+                if (rowCount === 4) {
+                    return true
+                }
+            } else {
+                rowCount = 0
+            }
+    }
+    for (var increment = 0; increment <= 12; increment += 6) {
+        var rowCount = 0
+        for (var i = 11 + increment; i <= 36 + increment / 6; i += 5)
+            if (gameState.board[i] == gameState.currentPlayer) {
+                rowCount++
+                if (rowCount === 4) {
+                    return true
+                }
+            } else {
+                rowCount = 0
+            }
+    }
+    return false
+}
+
+function checkDiagonalLeft(gameState: GameState): boolean {
+    for (var increment = 0; increment <= 2; increment++) {
+        var rowCount = 0
+        for (var i = 2 - increment; i <= 23 + increment * 6; i += 7)
+            if (gameState.board[i] == gameState.currentPlayer) {
+                rowCount++
+                if (rowCount === 4) {
+                    return true
+                }
+            } else {
+                rowCount = 0
+            }
+    }
+    for (var increment = 0; increment <= 12; increment += 6) {
+        var rowCount = 0
+        for (var i = 6 + increment; i <= 41 - increment / 6; i += 7)
+            if (gameState.board[i] == gameState.currentPlayer) {
+                rowCount++
+                if (rowCount === 4) {
+                    return true
+                }
+            } else {
+                rowCount = 0
+            }
+    }
+    return false
+}
